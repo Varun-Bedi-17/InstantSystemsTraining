a61a5a9b20c45f5beb6db40579823a4507d878b1
Varun-Bedi-17 <varunbedi02@gmail.com>
2024-06-04 17:15:27 +0530

    Some change in treemap inside collections class

--- KotlinPractice/src/main/kotlin/listandtreemap/Collections.kt
+++ KotlinPractice/src/main/kotlin/listandtreemap/Collections.kt
@@ -43,14 +43,32 @@ fun main() {
 //    alphabets2[2]= 3
 //    print(alphabets2)
 
+    // Treemap conatins sorted key-value pairs. We can provide comparator to change sorted behaviour.
     val alphabets : TreeMap<Any, Any> = TreeMap()
-    alphabets[1] = "A"
     alphabets[2] = "B"
+    alphabets[1] = "A"
+
+    // Sort automatically
     println(alphabets)
 
     for((word,alphabet) in alphabets)
     {
         println("$word for $alphabet")
     }
+
+    println(alphabets.keys)
+    println(alphabets.values)
+    println(alphabets.getOrDefault(2, "E"))
+    println(alphabets.containsKey(4))
+    println(alphabets.containsValue("B"))
+
+    println(alphabets.putIfAbsent(3,"C"))
+    println(alphabets)
+
+    println(alphabets.putAll(mapOf(7 to "G", 5 to "E", 4 to "D", 6 to "G")))
+    println(alphabets)
+
+    println(alphabets.remove(6))
+    println(alphabets)
 
 }
